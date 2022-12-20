3e612fecd38e49e60894ba967e7fe4194e2652a0
Taranchuk <nfhfyxer@gmail.com>
2022-12-20 21:51:35 +0200

    update

---
+++ 1.4/Source/EncounterFramework/HarmonyPatches/RerouteModdedGenStepsGenerate.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace EncounterFramework
+{
+    [HarmonyPatch]
+    public static class RerouteModdedGenStepsGenerate
+    {
+        public static List<MethodBase> targetMethods = new List<MethodBase>();
+        public static bool Prepare()
+        {
+            TryAddMethod("LargeFactionBase.GenStep_LargeFactionBase:Generate");
+            if (ModsConfig.IsActive("Torann.RimWar"))
+            {
+                TryAddMethod("LargeFactionBase.GenStep_LargeFactionBase2:Generate");
+            }
+            return targetMethods.Count > 0;
+        }
+
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            foreach (var method in targetMethods)
+            {
+                yield return method;
+            }
+        }
+
+        public static bool Prefix(Map __0, GenStepParams __1)
+        {
+            if (GenerationContext.customSettlementGeneration)
+            {
+                var genStep = new GenStep_Settlement();
+                genStep.Generate(__0, __1);
+                return false;
+            }
+            return true;
+        }
+        private static void TryAddMethod(string methodName)
+        {
+            var method = AccessTools.Method(methodName);
+            if (method != null)
+            {
+                targetMethods.Add(method);
+            }
+        }
+    }
+}
---
+++ 1.4/Source/EncounterFramework/HarmonyPatches/RerouteModdedGenStepsScatterAt.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace EncounterFramework
+{
+    [HarmonyPatch]
+    public static class RerouteModdedGenStepsScatterAt
+    {
+        public static List<MethodBase> targetMethods = new List<MethodBase>();
+
+        public static bool Prepare()
+        {
+            TryAddMethod("MapGenerator.Genstep_CreateBlueprintBase:ScatterAt");
+            TryAddMethod("PowerfulFactionBases.GenStep_Settlement:ScatterAt");
+            return targetMethods.Count > 0;
+        }
+
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            foreach (var method in targetMethods)
+            {
+                yield return method;
+            }
+        }
+
+        public static bool Prefix(IntVec3 __0, Map __1, GenStepParams __2, int __3 = 1)
+        {
+            if (GenerationContext.customSettlementGeneration)
+            {
+                var genStep = new GenStep_Settlement();
+                genStep.ScatterAt(__0, __1, __2, __3);
+                return false;
+            }
+            return true;
+        }
+        private static void TryAddMethod(string methodName)
+        {
+            var method = AccessTools.Method(methodName);
+            if (method != null)
+            {
+                targetMethods.Add(method);
+            }
+        }
+    }
+}
